cb1dfb63bd99e162776813fce488b7e5b179d910
Rajkamal <rajkamal.smit@gmail.com>
2021-07-07 20:48:09 +0530

    updated version

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
+import { useForm } from "react-hook-form";
 
-function App() {
-  return <div className="App">hello</div>;
+export default function App() {
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
+  console.log("error", errors);
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input
+        {...register("email", {
+          required: "email requierd",
+          pattern: {
+            value: /^\S+@\S+$/i,
+            message: "This is not a valid email",
+          },
+        })}
+      />
+      <span>{errors.email.message}</span>
+
+      <input {...register("lastName", { required: true })} />
+      {errors.lastName && "Last name is required"}
+
+      <input type="submit" />
+    </form>
+  );
 }
-
-export default App;
